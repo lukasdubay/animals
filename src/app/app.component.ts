ab214ebf00197ae457b8e59182c559ecaf6669e7
Daniel <me@danielmajar.cz>
2017-10-30 22:32:42 +0100

    new canvas approach

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,42 +1,11 @@
 import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { NativeAudio } from '@ionic-native/native-audio';
-import { Storage } from '@ionic/storage';
-
-import { MainPage } from '../pages/home/home';
-import { BackgroundProvider } from '../providers/background/background';
-
 @Component({
   templateUrl: 'app.html',
-  providers: [BackgroundProvider]
 })
 export class AnimalsApp {
-  rootPage:any = MainPage;
+  rootPage:any = '';
 
-  constructor(platform: Platform, storage: Storage, statusBar: StatusBar, splashScreen: SplashScreen, private background:BackgroundProvider, private nativeAudio: NativeAudio) {
-    platform.ready().then(() => {
-
-      background.setLoadedApp('loaded');
-      statusBar.overlaysWebView(true);
-      statusBar.hide();
-      splashScreen.hide();
-
-      storage.length().then((val) => {
-        console.log('storage: ',val);
-         if(val == 0){
-            storage.set('tutorial', false);
-            storage.set('congratulation', false);
-         }
-      });
-      if(platform.is('cordova')){
-
-        this.nativeAudio.preloadComplex('introM', 'audio/intro.mp3', 0.1, 1, 2).then(() => {
-          this.nativeAudio.loop('introM');
-        });
-        this.nativeAudio.preloadComplex('ambientM', 'audio/ambient.mp3', 0.1, 1, 0);
-      }
-    });
+  constructor() {
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,42 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { StatusBar } from '@ionic-native/status-bar';
-import { NativeAudio } from '@ionic-native/native-audio';
-import { IonicStorageModule } from '@ionic/storage';
 
 import { AnimalsApp } from './app.component';
 
-import { MainPage } from '../pages/home/home';
-import { GamePage } from '../pages/game/game';
 
-import { BackgroundProvider } from '../providers/background/background';
 
 @NgModule({
   declarations: [
     AnimalsApp,
-    MainPage,
-    GamePage,
   ],
   imports: [
     BrowserModule,
     IonicModule.forRoot(AnimalsApp),
-    IonicStorageModule.forRoot()
-
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     AnimalsApp,
-    MainPage,
-    GamePage,
   ],
   providers: [
-    StatusBar,
-    SplashScreen,
-    NativeAudio,
     {provide: ErrorHandler, useClass: IonicErrorHandler},
-    BackgroundProvider
   ]
 })
 export class AppModule {}
